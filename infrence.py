27519d76008f4becb20610f296f6c5ee50235757
bkazemivash <bkazemivash1@gsu.edu>
2023-03-06 22:04:53 +0000

    Updated inference module

--- infrence.py
+++ infrence.py
@@ -3,6 +3,7 @@ sys.path.append(os.path.dirname(__file__))
 
 import torch
 from torch.nn import CosineSimilarity
+from torch.utils.data import DataLoader
 from omegaconf import OmegaConf
 from lib.data_io import ScepterViTDataset
 from recognition.spatiotemporal_vit import VisionTransformer
@@ -19,8 +20,8 @@ def criterion(x1: torch.Tensor, x2: torch.Tensor, task='Recognition') -> torch.T
         torch.Tensor: Evaluation of given tensors.
     """ 
     if task == 'Recognition':
-        accuracy_ = CosineSimilarity(dim=1, eps=1e-6)
-        return accuracy_(x1, x2)
+        _,target = x1.topk(1, dim=1)
+        return torch.sum(target == x2)
     else:
         cos = CosineSimilarity(dim=1, eps=1e-6)
         pearson = cos(x1 - x1.mean(dim=1, keepdim=True), x2 - x2.mean(dim=1, keepdim=True))
@@ -56,21 +57,34 @@ def main():
     conf = OmegaConf.load(args.config)
     checkpoint = torch.load(args.pretrained_model)
     mask_file_path = os.path.abspath(args.mask)
-    dataset_file = os.path.abspath(args.dataset)
+    dataset_file = os.path.abspath(args.testset)
+    experiment_tag = conf.EXPERIMENT.tag
     logging.info("Loading test dataset for evaluating pretrained model.")
     main_dataset = ScepterViTDataset(image_list_file=dataset_file,
                                      mask_file=mask_file_path,
                                      **conf.DATASET)
+    dataloader = DataLoader(main_dataset, batch_size=conf.TEST.test_size, shuffle=conf.TEST.shuffling, num_workers=conf.TEST.test_workers, pin_memory=True)
     logging.info("Loading model and optimizer parameters.")
     pretrained_mdl = VisionTransformer(n_timepoints=main_dataset.time_bound, **conf.MODEL)
     if torch.cuda.is_available():
         pretrained_mdl = pretrained_mdl.cuda()    
     optimizer = torch.optim.Adam(pretrained_mdl.parameters())
-    pretrained_mdl.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-
-
-
+    pretrained_mdl.load_state_dict(checkpoint['state_dict'], strict=False)
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    logging.info('Changing model status to evaluation.')
+    pretrained_mdl.eval()
+    running_metric = 0.0
+    for batch_index, data in enumerate(dataloader, 0):
+        inp, label = data
+        inp = inp.to(dev, non_blocking=True)
+        label = label.to(dev, non_blocking=True)
+        with torch.set_grad_enabled(False):
+            preds = pretrained_mdl(inp)
+            metric = criterion(preds, label, experiment_tag)
+            logging.info(f'Evaluation on batch_{batch_index} - {conf.TEST.metric} : {metric}')
+            running_metric += metric.item()      
+    averaged_metric = running_metric / len(main_dataset.info_dataframe)
+    logging.info(f'Evaluation on average ::: {conf.TEST.metric} : {averaged_metric}')
 
 
 if __name__ == "__main__":
