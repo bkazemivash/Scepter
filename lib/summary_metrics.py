22faece1e4113fe054036e56e5c3f7804caeb651
bkazemivash <bkazemivash1@gsu.edu>
2024-01-04 19:18:20 +0000

    adding voxel-based IOU metric

--- lib/summary_metrics.py
+++ lib/summary_metrics.py
@@ -1,6 +1,56 @@
+"""
+This module includes evaluation methods for different scenarios like 
+dense prediciton or recognition.
+"""
+
 import torch
 import torch.nn as nn
 from torch.nn import CosineSimilarity
+from multipledispatch import dispatch
+
+
+
+def iou_3D(outputs: torch.Tensor, labels: torch.Tensor, thr = (.5, .5), smooth_ratio = 1e-6) -> float:
+    """Computes Intersection Over Union (IOU) metric based on number of 
+        voxels in intersection and union of ROI. Masking threshold is used
+        to seperated forground from background.
+
+    Args:
+        outputs (torch.Tensor): 3D tensor including predicted score map (x,y,z).
+        labels (torch.Tensor): 3D tensor including prior map  (x,y,z).
+        thr (tuple, optional): Masking threshold for output and label. Defaults to (.5, .5).
+        smooth_ratio (_type_, optional): Epsilon value to prevent devided by zero case. Defaults to 1e-6.
+
+    Returns:
+        float: Voxel-wise IOU of 2 maps.
+    """
+    assert (outputs.ndim == 3) and (labels.ndim ==3), '3D tensors are expected while inputs have different dimension.'
+    outputs = (outputs>thr[0]).int()
+    labels = (labels>thr[1]).int()
+    intersection = (outputs & labels).float().sum()  
+    union = (outputs | labels).float().sum()            
+    iou = (intersection + smooth_ratio) / (union + smooth_ratio)      
+    
+    return iou  
+
+
+def robust_scaling(x1: torch.Tensor):
+    """Implentation of Robust Scaling method - a median-based scaling method.
+
+    Args:
+        x1 (torch.Tensor): Input tensor.
+
+    Returns:
+        torch.Tensor: Scaled tensor.
+    """
+    Q1 = torch.quantile(x1, .25, dim=0,)
+    Q3 = torch.quantile(x1, .75, dim=0,)
+    M,_ = torch.median(x1, dim=0,)
+    IQR = Q3 - Q1
+    scaled_data = (x1 - M.squeeze(0)) / IQR.squeeze(0)
+    
+    return scaled_data
+
 
 def compute_accuracy(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     """Computes the top 1 accuracy on given tensors.
@@ -16,6 +66,7 @@ def compute_accuracy(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     _,target = x1.topk(1, dim=1)
     return torch.sum(target == x2)
 
+
 def compute_correlation(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     """Computes the correlation between given tensors.
     
@@ -30,6 +81,7 @@ def compute_correlation(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
     cos = CosineSimilarity(dim=1, eps=1e-6)
     pearson = cos(x1 - x1.mean(dim=1, keepdim=True), x2 - x2.mean(dim=1, keepdim=True))
     return pearson
+
 
 def compute_confusion_matrix(x1: torch.Tensor, 
                              x2: torch.Tensor, 
@@ -53,23 +105,3 @@ def compute_confusion_matrix(x1: torch.Tensor,
     for t, p in zip(target.view(-1), x2.view(-1)):
             confusion_matrix[t.long(), p.long()] += 1      
     return confusion_matrix
-
-def compute_roc_curve(x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
-    return torch.rand(1)
-
-def robust_scaling(x1: torch.Tensor):
-    """Implentation of Robust Scaling method - a median-based scaling method.
-
-    Args:
-        x1 (torch.Tensor): Input tensor.
-
-    Returns:
-        torch.Tensor: Scaled tensor.
-    """
-    Q1 = torch.quantile(x1, .25, dim=0,)
-    Q3 = torch.quantile(x1, .75, dim=0,)
-    M,_ = torch.median(x1, dim=0,)
-    IQR = Q3 - Q1
-    scaled_data = (x1 - M.squeeze(0)) / IQR.squeeze(0)
-    
-    return scaled_data
